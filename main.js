a8b74b69e43ccdbe23d9a5903ceca1ec92547077
Luis Fernando santos <142061648+Luis-1A@users.noreply.github.com>
2025-05-09 17:28:27 -0300

    bbb

--- main.js
+++ main.js
@@ -22,10 +22,10 @@ header = header.innerHTML = `<div class="menu-area">
                 <a href="index.html">
                     <li>Início</li>
                 </a>
-                <a href="about.html">
+                <a href="serviços.html">
                     <li>Serviços</li>
                 </a>
-                <a href="sobre.html" target="_blank">
+                <a href="about.html" target="_blank">
                     <li>Sobre</li>
                 </a>
                 <a href="https://wa.me/5561993715768?text=Ol%C3%A1!%20Vi%20algumas%20informa%C3%A7%C3%B5es%20no%20site%20e%20gostaria%20de%20saber%20mais" target="_blank">
